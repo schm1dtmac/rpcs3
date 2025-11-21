e151a4359edd1c222c97ce36ac3730137a99e7bd
schm1dtmac <schm1dtxbox@icloud.com>
2025-11-21 21:09:17 +0000

    Install MVK ICD into macOS bundle

--- .ci/deploy-mac-arm64.sh
+++ .ci/deploy-mac-arm64.sh
@@ -15,6 +15,10 @@ echo "AVVER=$AVVER" >> ../.ci/ci-vars.env
 
 cd bin
 mkdir "rpcs3.app/Contents/lib/" || true
+mkdir -p "rpcs3.app/Contents/Resources/vulkan/icd.d" || true
+wget https://github.com/KhronosGroup/MoltenVK/releases/download/v1.4.0/MoltenVK-macos.tar
+tar xvf ./MoltenVK-macos.tar
+cp -R "./MoltenVK/MoltenVK/dynamic/dylib/macOS/" "rpcs3.app/Contents/Resources/vulkan/icd.d"
 
 cp "$(realpath /opt/homebrew/opt/llvm@$LLVM_COMPILER_VER/lib/c++/libc++abi.1.0.dylib)" "rpcs3.app/Contents/Frameworks/libc++abi.1.dylib"
 cp "$(realpath /opt/homebrew/lib/libsharpyuv.0.dylib)" "rpcs3.app/Contents/lib/libsharpyuv.0.dylib"
